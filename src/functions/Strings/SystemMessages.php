ca8fd33f22177685a66f2a31001d9bfb9ef43bad
Jeff Jones <jeff@socalbioinformatics.com>
2018-04-14 16:30:49 -0700

    removed redundant messaging functions

--- src/functions/Strings/SystemMessages.php
+++ src/functions/Strings/SystemMessages.php
@@ -74,40 +74,4 @@ function systemMessage($message)
         echo "</PRE>";
 }
 
-//
-function print_message($variable, $value = null, $sep = ".", $len = 75, $echo = true)
-{
-    $out = str_pad("$variable ", $len, $sep) . " $value\n";
-    
-    if ($echo == true)
-        echo $out;
-    
-    return $out;
-}
-
-function print_var($variable, $web = true)
-{
-    if (is_true($web))
-        echo "<br><pre>";
-    
-    if (is_array($variable)) {
-        print_r($variable);
-    } else {
-        echo $variable . PHP_EOL;
-    }
-    
-    if (is_true($web))
-        echo "</pre>";
-}
-
-//
-function print_setting($variable = null, $value = null, $sep = " ", $len = 30, $echo = true)
-{
-    $out = str_pad("$variable :", $len, $sep, STR_PAD_LEFT) . " $value\n";
-    
-    if ($echo == true)
-        echo $out;
-    
-    return $out;
-}
 ?>
