f63e45a827dc3ef4393c386bb3b330d0fe7cd4a2
Jeff Jones <jeff@socalbioinformatics.com>
2019-04-30 15:01:26 -0700

    update email validate

---
+++ src/functions/Strings/email_validate.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * Written by Jeff Jones (jeff@socalbioinformatics.com)
+ * Copyright (2016) SoCal Bioinformatics Inc.
+ *
+ * See LICENSE.txt for the license.
+ */
+function email_validate(string $email)
+{
+    if (! filter_var($email, FILTER_VALIDATE_EMAIL))
+        return false;
+    /*
+     * clean email input
+     */
+    $email = filter_var(strtolower($email), FILTER_SANITIZE_EMAIL);
+    preg_match("/.+\@/", $email, $email_id);
+    $email_sb = str_replace(".", "", $email_id[0]);
+    $email = preg_replace("/" . $email_id[0] . "/", $email_sb, $email);
+    
+    return $email;
+}
+
+?>
