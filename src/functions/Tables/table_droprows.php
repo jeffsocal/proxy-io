9893a82b32a018c58b987a8f9ff05744d5a7bf74
jeffsocal <jeff@socalbioinformatics.com>
2019-10-24 20:42:55 -0700

    updates

--- src/functions/Tables/table_droprows.php
+++ src/functions/Tables/table_droprows.php
@@ -15,21 +15,48 @@ function table_droprows($table_array, $rows)
     /*
      * quiet exit if ROW is not present
      */
-    if(is_null($rows))
+    if (is_null($rows))
         return $table_array;
-    
+
     if (! is_array($rows))
         $rows = array(
             $rows
         );
     $rows = array_flip($rows);
-    
+
     $table_header = table_header($table_array);
-    
+
     foreach ($table_header as $table_header_name) {
         $table_array[$table_header_name] = array_diff_key($table_array[$table_header_name], $rows);
     }
     $table_array = table_reindex($table_array);
     return $table_array;
 }
+
+/*
+ *
+ */
+function table_keeprows($table_array, $rows)
+{
+    /*
+     * quiet exit if ROW is not present
+     */
+    if (is_null($rows))
+        return $table_array;
+
+    if (! is_array($rows))
+        $rows = array(
+            $rows
+        );
+    $rows = array_flip($rows);
+
+    $table_header = table_header($table_array);
+
+    foreach ($table_header as $table_header_name) {
+        $table_array[$table_header_name] = array_intersect_key($table_array[$table_header_name], $rows);
+    }
+    $table_array = table_reindex($table_array);
+    return $table_array;
+}
+
 ?>
